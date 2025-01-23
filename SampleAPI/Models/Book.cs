01ad60bcc582e9ddb8066dacb56c68de6005bc34
Ha Anh Duy <duyha10092004@gmail.com>
2025-01-23 09:04:12 +0700

    Initial commit

---
+++ SampleAPI/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+namespace SampleAPI.Models
+{
+    public class Book
+    {
+        [BsonId] //primary key
+        [BsonRepresentation(BsonType.ObjectId)] //passing paran as string
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+
+        public string BookName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+    }
+}
